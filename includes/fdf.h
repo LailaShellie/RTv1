c8b30b11c68a190cfc988a3c9d16935e687a2734
Sem Begunov <smbeg@mail.ru>
2020-02-25 21:59:49 +0300

    add keycodes in fdf.h

--- includes/fdf.h
+++ includes/fdf.h
@@ -13,6 +13,9 @@
 # define FDF_KEY_NP_MINUS 78
 # define FDF_SCROLL_U 4
 # define FDF_SCROLL_D 5
+
+# define FDF_MOUSE_LEFT 1
+
 # define FDF_KEY_1 18
 # define FDF_KEY_2 19
 # define FDF_KEY_3 20
@@ -20,21 +23,37 @@
 # define FDF_KEY_5 23
 # define FDF_KEY_6 22
 # define FDF_KEY_7 26
-# define FDF_MOUSE_LEFT 1
+# define FDF_KEY_8 28
+# define FDF_KEY_9 25
+# define FDF_KEY_0 29
+
 # define FDF_KEY_A 0
+# define FDF_KEY_B 11
 # define FDF_KEY_C 8
+# define FDF_KEY_D 2
+# define FDF_KEY_E 14
+# define FDF_KEY_F 3
+# define FDF_KEY_G 5
+# define FDF_KEY_H 4
+# define FDF_KEY_I 34
+# define FDF_KEY_J 38
+# define FDF_KEY_K 40
+# define FDF_KEY_L 37
+# define FDF_KEY_M 46
+# define FDF_KEY_N 45
+# define FDF_KEY_O 31
+# define FDF_KEY_P 35
 # define FDF_KEY_Q 12
+# define FDF_KEY_R 15
+# define FDF_KEY_S 1
+# define FDF_KEY_T 17
+# define FDF_KEY_U 32
+# define FDF_KEY_V 9
 # define FDF_KEY_W 13
 # define FDF_KEY_X 7
-# define FDF_KEY_S 1
-# define FDF_KEY_Z 6
-# define FDF_KEY_D 2
-# define FDF_KEY_U 32
-# define FDF_KEY_J 38
-# define FDF_KEY_H 4
-# define FDF_KEY_K 40
 # define FDF_KEY_Y 16
-# define FDF_KEY_I 34
+# define FDF_KEY_Z 6
+
 # define FDF_Z 6
 
 #endif
